1286ed51bf1490bb6657a0d0e6e5c50bbd7548cc
Jammy <jamjamfr@proton.me>
2025-04-28 11:12:57 +0100

    Update ColorSensorComponent.java
    Fixed bot travel.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorComponent.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
@@ -10,9 +11,33 @@ public class ColorSensorComponent {
     private final NormalizedColorSensor colorSensor;
     boolean moving;
 
+    double travelDistance;
+
+    double currentDistance;
+
+    private MecanumDrive drive;
+
+    double FinalEX;
+
     public ColorSensorComponent(HardwareMap hardwareMap, String SensorID) {
         colorSensor = (NormalizedColorSensor) hardwareMap.colorSensor.get(SensorID);
         colorSensor.setGain(2);
+    }
+
+    public void runComponent(){
+        while(moving){
+            for (double CurrentEX = 0; CurrentEX <= FinalEX;) {
+                while(currentDistance<=travelDistance) {
+                    drive.driveRobotCentric(-2, 0, 0);
+                    currentDistance+=2;
+                }
+                while(currentDistance<=travelDistance) {
+                    drive.driveRobotCentric(2, 0, 0);
+                    currentDistance+=2;
+                }
+            }
+            moving = false;
+        }
     }
 
     public void blueCheck(){
