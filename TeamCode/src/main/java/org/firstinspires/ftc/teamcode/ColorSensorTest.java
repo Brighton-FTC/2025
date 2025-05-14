12d32abd00bde1472d97a4f444eb3479a014c993
Phyeory <jamjamfr@proton.me>
2025-05-14 21:26:04 +0100

    OpenCV Opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -3,18 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
 import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 @TeleOp
 class ColorSensorTester extends OpMode {
     private ColorSensorComponent sensor;
 
-    private MecanumDrive drive;
-    private IMU imu;
 
     GamepadEx gamePad;
 
@@ -22,33 +16,6 @@ class ColorSensorTester extends OpMode {
     public void init() {
         sensor = new ColorSensorComponent(hardwareMap, "color-sensor");
         gamePad = new GamepadEx(gamepad1);
-
-        Motor[] motors = {
-                new Motor(hardwareMap, "front_left_drive"),
-                new Motor(hardwareMap, "front_right_drive"),
-                new Motor(hardwareMap, "back_left_drive"),
-                new Motor(hardwareMap, "back_right_drive")
-        };
-
-        for (Motor motor : motors) {
-            motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        }
-
-        motors[2].setInverted(true);
-
-        drive = new MecanumDrive(motors[0], motors[1], motors[2], motors[3]);
-
-
-        imu = hardwareMap.get(IMU.class, "imu");
-        imu.initialize(new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-                )
-        ));
-
-        imu.resetYaw();
-
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTester.java
@@ -1,13 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
 
@@ -15,8 +11,6 @@ import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
 public class OpenCVTester extends OpMode {
 
     private OpenCVComponent sensor;
-
-    private MecanumDrive drive;
 
     GamepadEx gamePad;
 
