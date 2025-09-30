f2b18b0e14f9861638b2088e938cf6a66d75a473
Jaime Mok <jamjamfr@proton.me>
2025-09-30 09:33:05 +0100

    Enhance GrabberComponent with constants and comments
    Added CLAW_TURN_AMOUNT constant and improved comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GrabberComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GrabberComponent.java
@@ -5,19 +5,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
+//@Config allows real time optimisation towards variables,
 @Config
 public class GrabberComponent {
+    
     public static double CLAW_TURN_AMOUNT = 0.5;
 
+    //Initialising the hardware claw,
+    private final Servo claw; 
 
-    private final Servo claw;
-
-
+    //Create constructor for access in other classes or OpModes. (This is the base setting for the servo)
     public GrabberComponent(HardwareMap hardwareMap, String servoId) {
         claw = hardwareMap.servo.get(servoId);
         claw.setDirection(Servo.Direction.REVERSE);
     }
 
+    //Construct the method that acts on the servo. We call this by doing something.grab(), where something is initialised to 
     public void grab() {
         claw.setPosition(0);
 
